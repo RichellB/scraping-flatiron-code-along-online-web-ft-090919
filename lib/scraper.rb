9599c9ed3fff225b17659e82dd12dbb4496fa909
Richell Bonfield <richell.bonfield12@gmail.com>
2019-09-29 19:01:34 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,7 +7,10 @@ class Scraper
   
   def get_page
      doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
-   end
+  end
+  
+  def get_courses 
+    self.get_page.css(".post")
   end
   
   def print_courses
